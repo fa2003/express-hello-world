cbce8d80c8d1c2503ae5ae1d2db14aeb776463a3
Fabrizio Escolar <escolarfabrizio@gmail.com>
2020-06-25 14:05:42 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // index.js
+
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
